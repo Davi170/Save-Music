480f85aa0b9594744207a13ce1af591038122e25
Davi170 <103954717+Davi170@users.noreply.github.com>
2022-10-26 16:59:14 -0300

    Update SpotifyController.cs

--- back/Controllers/SpotifyController.cs
+++ back/Controllers/SpotifyController.cs
@@ -9,11 +9,14 @@ public class SpotifyController : ControllerBase
     [HttpGet]
     public async Task<IActionResult> Get()
     {
+        var client_id = "c3f60b3448904bb388f72beee6fccff9";
+        var redirect_uri = "https://localhost:7231/";
+        
         HttpClient client = new HttpClient();
         client.BaseAddress = new Uri("https://accounts.spotify.com");
         var result = await client.GetAsync(
-            "/authorize?response_type=code&client_id=${this.clientID}&" + 
-            "scope=${this.scopes}-read-private user-read-email&redirect_uri=${this.redirectURI}&" +
+            "/authorize?response_type=code&client_id=c3f60b3448904bb388f72beee6fccff9&" + 
+            "scope=playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify-public-read-private user-read-email&redirect_uri=https://localhost:7231/&" +
             "state=CARACTERESALEATORIOS");
     }
 

79c59ff831af175dacbfc3c222a229386455a349
unknown <davisousa1703@gmai.com>
2022-11-17 16:38:10 -0300

    token

--- back/Controllers/SpotifyController.cs
+++ back/Controllers/SpotifyController.cs
@@ -14,14 +14,17 @@ public class SpotifyController : ControllerBase
         
         HttpClient client = new HttpClient();
         client.BaseAddress = new Uri("https://accounts.spotify.com");
+        var state = "daviDAVId4v1ivad"
+        var callback = "https://localhost:7231/callback"
         var result = await client.GetAsync(
             "https://accounts.spotify.com/authorize?response_type=code&client_id=c3f60b3448904bb388f72beee6fccff9&" + 
-            "scope=playlist-read-collaborative playlist-modify-public playlist-read-private playlist-modify-private&redirect_uri=https://localhost:7231/playlists&");
+            "scope=playlist-read-collaborative playlist-modify-public playlist-read-private playlist-modify-private&redirect_uri=" + callback + "&" +
+            "state=" + state);
             return Ok();
     }
 
     [HttpGet("callback")]
-    public async Task<IActionResult> Callback()
+    public async Task<IActionResult> Callback(string code, string state)
     {
         return Ok();
     }

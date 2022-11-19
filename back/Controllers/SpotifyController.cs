75e7700ab880cbe3e721f731bee64d9a5c8f2f95
Davi170 <103954717+Davi170@users.noreply.github.com>
2022-11-19 20:42:18 -0300

    Back para front

--- back/Controllers/SpotifyController.cs
+++ back/Controllers/SpotifyController.cs
@@ -10,12 +10,12 @@ public class SpotifyController : ControllerBase
     public async Task<IActionResult> Get()
     {
         var client_id = "c3f60b3448904bb388f72beee6fccff9";
-        var redirect_uri = "https://localhost:7231/playlists";
+        var redirect_uri = "https://localhost:7008/playlists";
         
         HttpClient client = new HttpClient();
         client.BaseAddress = new Uri("https://accounts.spotify.com");
-        var state = "daviDAVId4v1ivad"
-        var callback = "https://localhost:7231/callback"
+        var state = "daviDAVId4v1ivad";
+        var callback = "https://localhost:7008/callback";
         var result = await client.GetAsync(
             "https://accounts.spotify.com/authorize?response_type=code&client_id=c3f60b3448904bb388f72beee6fccff9&" + 
             "scope=playlist-read-collaborative playlist-modify-public playlist-read-private playlist-modify-private&redirect_uri=" + callback + "&" +

9a1daa882012faedf603bb41b3a464ad6d156df7
Davi170 <103954717+Davi170@users.noreply.github.com>
2022-11-28 16:52:33 -0300

    Update SpotifyController.cs

--- back/Controllers/SpotifyController.cs
+++ back/Controllers/SpotifyController.cs
@@ -6,26 +6,19 @@ namespace back.Controllers;
 [Route("Login")]
 public class SpotifyController : ControllerBase
 {
+    
+    
     [HttpGet]
     public async Task<IActionResult> Get()
     {
-        var client_id = "c3f60b3448904bb388f72beee6fccff9";
-        var redirect_uri = "https://localhost:7008/playlists";
         
-        HttpClient client = new HttpClient();
-        client.BaseAddress = new Uri("https://accounts.spotify.com");
-        var state = "daviDAVId4v1ivad";
-        var callback = "https://localhost:7008/callback";
-        var result = await client.GetAsync(
-            "https://accounts.spotify.com/authorize?response_type=code&client_id=c3f60b3448904bb388f72beee6fccff9&" + 
-            "scope=playlist-read-collaborative playlist-modify-public playlist-read-private playlist-modify-private&redirect_uri=" + callback + "&" +
-            "state=" + state);
             return Ok();
     }
 
     [HttpGet("callback")]
     public async Task<IActionResult> Callback(string code, string state)
     {
+        
         return Ok();
     }
 }
